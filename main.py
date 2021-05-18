3794919d06e46c770d3c23f3c286712fee596abb
TomasStulrajter <parchant99@gmail.com>
2021-05-18 15:15:04 +0200

    Konverzia dicom na png a vyrabanie custom vyrezov roi

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import pydicom
 import pydicom.data
 import csv
 import os
+from PIL import Image
+import numpy
 
 #-----------------------------------------
 
@@ -10,8 +12,12 @@ import os
 mass_training_base_address = 'D:/Downloads/3.rocnik/Prax/Database/Mass training/'
 mass_training_csv_full_address = 'D:/Downloads/3.rocnik/Prax/Database/Mass training set csv metadata/mass_case_description_train_set.csv'
 
+#base adress values for folders for malignant and benign image sets
+malignant_base_address = 'D:/Downloads/3.rocnik/Prax/Database/Malignant benign image sets/MALIGNANT/'
+benign_base_address = 'D:/Downloads/3.rocnik/Prax/Database/Malignant benign image sets/BENIGN/'
+
 first_record = 1
-last_record = 2
+last_record = 21
 max_record = 0
 
 #FUNCTIONS
@@ -178,6 +184,7 @@ def draw_everything_one_cycle(mammogram_pixel_array, roi_pixel_array):
             else:
                 j = j + 1
 
+    print(bottom, top, left, right)
     #minor cycles
     #draw bounding box
     #top line && bottom line
@@ -196,8 +203,93 @@ def draw_everything_one_cycle(mammogram_pixel_array, roi_pixel_array):
         for j in range(-pencil_width, pencil_width):
             mammogram_pixel_array[i][left + j] = pencil_color
 
-    #DISPLAY RESULT
+
+    # DISPLAY RESULT
     draw_with_description(mammogram_pixel_array)
+
+
+def create_bounding_box(mammogram_pixel_array, bottom, top, left, right):
+    bounding_box_image = numpy.arange((bottom - top) * (right - left))
+    index = 0
+    for x in range(top, bottom):
+        for y in range(left, right):
+            bounding_box_image[index] = mammogram_pixel_array[x][y]
+            index += 1
+
+    bounding_box_image = numpy.reshape(bounding_box_image, ((bottom - top), (right - left)))
+    print(bounding_box_image.shape)
+    image = Image.fromarray(bounding_box_image)
+    image.save('test.png')
+
+
+def create_bounding_box_normalised(mammogram_pixel_array, roi_pixel_array, height, width,record_number):
+    bottom = 0
+    top = 0
+    left = roi_pixel_array.shape[1] - 1
+    right = 0
+
+    top_found = 0
+    bottom_found = 1
+
+    #detect bounding box position
+    # scan all rows
+    for i in range(roi_pixel_array.shape[0]):
+        j = 0
+        while j < roi_pixel_array.shape[1]:
+
+            # 1st non-zero pixel found in mask row
+            if roi_pixel_array[i][j] != 0:
+                if top_found == 0:
+                    top = i
+                    top_found = 1
+                if roi_pixel_array[i + 1][j] != 0:
+                    bottom_found = 0
+                if j < left:
+                    left = j
+
+                # scanning non-zero pixels
+                while roi_pixel_array[i][j] != 0:
+                    j = j + 1
+
+                # last non-zeor pixel found
+                if bottom_found == 1:
+                    bottom = i + 1
+                else:
+                    bottom_found = 1
+                if j > right:
+                    right = j
+            else:
+                j = j + 1
+
+    print(bottom, top, left, right)
+
+    #find center and determine new bounding box position
+    center_x = round((left + right) / 2)
+    center_y = round((top + bottom) / 2)
+
+    new_bottom = round(center_y + (height / 2))
+    new_top = round(center_y - (height / 2))
+    new_left = round(center_x - (width / 2))
+    new_right = round(center_x + (width / 2))
+
+    #create new image
+    bounding_box_image = numpy.arange((new_bottom - new_top) * (new_right - new_left))
+    index = 0
+    for x in range(new_top, new_bottom):
+        for y in range(new_left, new_right):
+            bounding_box_image[index] = mammogram_pixel_array[x][y]
+            index += 1
+
+    bounding_box_image = numpy.reshape(bounding_box_image, ((new_bottom - new_top), (new_right - new_left)))
+    print(bounding_box_image.shape)
+    image = Image.fromarray(bounding_box_image)
+
+    status = metadata[record_number][9]
+    if status == "MALIGNANT":
+        image.save(malignant_base_address + 'malignant' + str(record_number) + '.png')
+    elif status == "BENIGN":
+        image.save(benign_base_address + 'benign' + str(record_number) + '.png')
+
 
 
 #MAIN
@@ -273,7 +365,8 @@ for i in range(first_record, last_record):
 
     #draw_roi(mammogram_pixel_array, roi_pixel_array)
     #draw_bounding_box(mammogram_pixel_array, roi_pixel_array)
-    draw_everything_one_cycle(mammogram_pixel_array, roi_pixel_array)
+    #draw_everything_one_cycle(mammogram_pixel_array, roi_pixel_array)
+    create_bounding_box_normalised(mammogram_pixel_array, roi_pixel_array, 300, 300, i)
 
 
 #call the methods and assigning arrays to data sets
